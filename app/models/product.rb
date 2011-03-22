eed9c8e94839323bbf4f30687068592697976ea8
Marius ALbath <marius.albath@gmail.com>
2011-03-22 15:53:47 +0100

    shitty

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+
+validates :title, :description, :image_url, :price,  :presence => true
+
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+
+test "product attributes must not be empty" do
+	product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
 end
