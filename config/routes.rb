b90f31543b550ea9782accc9160729eda01b87ca
Marius ALbath <marius.albath@gmail.com>
2011-03-29 15:45:40 +0200

    lliHallo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Webshop::Application.routes.draw do
+  get "store/index"
+
   resources :products
+root :to => 'store#index', :as => 'store'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,6 +54,7 @@ Webshop::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+#root :to => 'store#index'
 
   # See how all your routes lay out with "rake routes"
 
