8cada8ff8fe0fa9140cba95016fd958c5d7419af
Marius ALbath <marius.albath@gmail.com>
2011-04-07 19:17:55 +0200

    redirect path added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,10 +43,10 @@ class LineItemsController < ApplicationController
 @cart = current_cart
 product = Product.find(params[:product_id])
 @line_item = @cart.add_product(product.id)
+
 respond_to do |format|
 if @line_item.save
-format.html { redirect_to(@line_item.cart,
-:notice => 'Line item was successfully created.') }
+format.html { redirect_to(store_url) }
 format.xml { render :xml => @line_item,
 :status => :created, :location => @line_item }
 else
