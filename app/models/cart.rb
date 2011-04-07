7e98c1c06a6dae027877981a58bcd7b634915841
Marius ALbath <marius.albath@gmail.com>
2011-04-07 18:04:43 +0200

    Cart added on frontpage

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,9 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, :dependent => :destroy
+def total_price
+line_items.to_a.sum { |item| item.total_price }
+end
+
 def add_product(product_id)
 current_item = line_items.find_by_product_id(product_id)
 if current_item
